1d7cecdf36f8ca2ad35fae6e778c3f18d8a61361
Nikit1063 <nikitashch2015@gmail.com>
2024-09-29 13:41:20 +0500

    Update AllClasses.py

--- AllClasses.py
+++ AllClasses.py
@@ -1,4 +1,11 @@
 import telebot
+import sqlite3
+
+minutes = ''
+houres = ''
+
+connection = sqlite3.connect('DataBase.db')
+cursor = connection.cursor()
 
 class Movies:
     def __init__(self, name, duringInMinutes) -> None:
@@ -27,85 +34,118 @@ class CinemaHall:
         self.moviesAndTime = moviesAndTime
 
     def __str__(self) -> str:
-        string = f"Кинозал: {self.name}\nКолличество мест: {self.row * self.column}\n\nКиносиансы:\n"
+        global houers
+        global minutes
+
+        string = f"Кинозал: {self.name}\nКолличество мест: {self.row * self.column}\n\nКиносеансы:\n"
+
         for key, value in self.moviesAndTime.items():
             minutes = int(key.split(":")[1])
             minutes = minutes + value.during % 60
+
             if len(str(minutes)) == 1:
                 minutes = str(minutes).replace(str(minutes), "0" + str(minutes))
+
             houers = int(key.split(":")[0])
             string = string + f"{value.name} - Начало: {key} - Конец: {houers + value.during // 60}:{minutes}\n({str(value)})\n"
+
         return string[0:len(string) - 1]
 
     def book(self, message, bot, time=None, move=None) -> None:
+        global houers
+        global minutes
+
         if time == None and move == None:
             moves = []
             times = []
             string = ""
+
             for key, value in self.moviesAndTime.items():
                 moves.append(value.name)
                 times.append(key)
                 minutes = int(key.split(":")[1])
                 minutes = minutes + value.during % 60
+
                 if len(str(minutes)) == 1:
                     minutes = str(minutes).replace(str(minutes), "0" + str(minutes))
                 houers = int(key.split(":")[0])
                 string = string + f"{value.name} - Начало: {key} - Конец: {houers + value.during // 60}:{minutes}\n({str(value)})\n"
             bot.send_message(chat_id=message.chat.id, text=string[0:len(string) - 1])
             d = []
+
             for i in move:
                 d.append(telebot.types.KeyboardButton(text=str(i)))
+
             moveKeybord = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
             moveKeybord.add(*d)
+
             bot.send_message(chat_id=message.chat.id, text="Введите название фильма: ", reply_markup=moveKeybord)
             move = message.text
+
             while not (move in moves):
                 bot.send_message(chat_id=message.chat.id, text="Сегодня такой фильм не показывают. Введите название фильма: ", reply_markup=moveKeybord)
                 move = message.text
+
             d = []
+
             for i in time:
                 d.append(telebot.types.KeyboardButton(text=str(i)))
+
             moveKeybord = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
             moveKeybord.add(*d)
-            bot.send_message(chat_id=message.chat.id, text="Выбирете время: ", reply_markup=moveKeybord)
+
+            bot.send_message(chat_id=message.chat.id, text="Выберите время: ", reply_markup=moveKeybord)
             time = message.text
+
             while not (time in times):
-                bot.send_message(chat_id=message.chat.id, text="Сегодня в это время фильм не показывают. Выбирете время: ", reply_markup=moveKeybord)
+                bot.send_message(chat_id=message.chat.id, text="Сегодня в это время фильм не показывают. Выберите время: ", reply_markup=moveKeybord)
                 time = message.text
         mainstring = len(str(self.column)) * " " + "  "
+
         for i in range(self.row):
             if i == 0:
                 mainstring += str(i + 1)
+
             else:
                 mainstring += str(((len(str(self.row)) - len(str(i))) * " " + "  ") + str((i + 1)))
                 mainstring += "\n"
+
         for i in range(self.column):
             for j in range(self.row):
+
                 if (move, i, j, time) in self.bookedSeates:
+
                     if j == 0:
                         mainstring += str(str(i + 1) + ((len(str(self.column)) - len(str(i + 1))) * " " + " |"))
                     mainstring += str("*" + (len(str(self.row)) * " " + " "))
+
                 else:
                     if j == 0:
                         mainstring += str(str(i + 1) + ((len(str(self.column)) - len(str(i + 1))) * " " + " |"))
-                    mainstring += str("o", + (len(str(self.row)) * " " + " "))
+                    mainstring += str("o" + (len(str(self.row)) * " " + " "))
             mainstring += "\n"
         mainstring += "* - место знято\n"
         mainstring += "o - место свободно\n"
+
         qwKeyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
         qwKeyboard.add(
             telebot.types.KeyboardButton(text='Да'),
             telebot.types.KeyboardButton(text='Нет')
         )
+
         bot.send_message(chat_id=message.chat.id, text=mainstring)
         bot.send_message(chat_id=message.chat.id, text="Продолжить выбор места?", reply_markup=qwKeyboard)
+
         target = message.text
+
         if target != "Да":
             return
+
         bot.send_message(chat_id=message.chat.id, text="Введите ряд: ")
         row = int(message.text)
         bot.send_message(chat_id=message.chat.id, text="Введите место: ")
         seat = int(message.text)
+
         while row > self.column or seat > self.row or row <= 0 or seat <= 0 or (
         move, seat, row, time) in self.bookedSeates:
             bot.send_message(chat_id=message.chat.id, text="Это место уже занято или его не сушествует")
@@ -113,6 +153,11 @@ class CinemaHall:
             row = int(message.text)
             bot.send_message(chat_id=message.chat.id, text="Введите место: ")
             seat = int(message.text)
+
+        else:
+            cursor.execute(f'''INSERT INTO 
+            users(id, row, place, time, film) 
+            VALUES({message.from_user.id}, {row}, {seat}, {houers}:{minutes}, {move})''')
         self.bookedSeates.append((move, row - 1, seat - 1, time))
 
     def cancelAllBooks(self) -> None:
@@ -180,6 +225,7 @@ class Cinema:
     def __str__(self) -> str:
         j = 1
         string = f"Кинотеатр: {self.name}\nКинозалы:\n\n"
+
         for i in self.halls:
             string = string + str(j) + ":" + "\n" + str(i) + "\n"
             j += 1
@@ -200,6 +246,7 @@ class Cinema:
         )
         bot.send_message(chat_id=message.chat.id, text="Введите сколько заллов хотите создать: ",
                          reply_markup=namberKeybord)
+
         for i in range(int(message.text)):
             d = createHall(bot, message)
             d.append(message, bot)
@@ -216,10 +263,14 @@ class Cinema:
     #         print("Такого кинозала не сушествует")
     #         hall = input("Введите название зала: ")
     #     self.halls[halls.index(hall)].book()
+
     def book(self, bot, message, menuKeyboard) -> None:
+        global value
+
         bot.send_message(chat_id=message.chat.id, text='Получить помощь системы в подборе фильма\nВыбрать фильм самостоятельно',
                          reply_markup=menuKeyboard)
         target = message.text()
+
         if target == "Да":
             b = []
             films = set()
@@ -229,92 +280,111 @@ class Cinema:
                     b.append((i, value, key))
                     films.add(value.name)
             d = []
+
             for i in films:
                 d.append(telebot.types.KeyboardButton(text=str(i)))
 
             keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
             keyboard.add(*d)
-            bot.send_message(chat_id=message.chat.id, text='Выбирите интересующий вас фильм:',
+            bot.send_message(chat_id=message.chat.id, text='Выберите интересующий вас фильм:',
                          reply_markup=keyboard)
             target1 = message.text
+
             while not (target1 in films):
-                bot.send_message(chat_id=message.chat.id, text='Такой фильм сегодня не показывают. Выбирите интересующий вас фильм:',
+                bot.send_message(chat_id=message.chat.id, text='Такой фильм сегодня не показывают. Выберите интересующий вас фильм:',
                                  reply_markup=keyboard)
                 target1 = message.text
             halls = set()
             string1 = ""
+
             for i in b:
                 if i[1].name == target1:
                     halls.add(i[0].name)
                     minutes = int(i[2].split(":")[1])
                     houers = int(i[2].split(":")[0])
                     minutes = minutes + value.during % 60
+
                     if len(str(minutes)) == 1:
                         minutes = str(minutes).replace(str(minutes), "0" + str(minutes))
                     string1 += (
                         f"Кинотеатр: {self.name}\nЗал: {i[0].name}\nВремя:{i[2]}-{houers + i[1].during // 60}:{minutes}\n")
             bot.send_message(chat_id=message.chat.id, text=string1)
             d = []
+
             for i in halls:
                 d.append(telebot.types.KeyboardButton(text=str(i)))
             keyboard2 = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
             keyboard2.add(*d)
             bot.send_message(chat_id=message.chat.id, text="Выбирите зал: ", reply_markup=keyboard2)
             target3 = message.text
+
             while not (target3 in halls):
                 bot.send_message(chat_id=message.chat.id, text="Такого зала не существует. Выбирите зал: ", reply_markup=keyboard2)
                 target3 = message.text
             time = set()
             string2 = ""
+
             for i in b:
                 if i[1].name == target1 and i[0].name == target3:
                     time.add(i[2])
                     minutes = int(i[2].split(":")[1])
+
                     if len(str(minutes)) == 1:
                         minutes = str(minutes).replace(str(minutes), "0" + str(minutes))
+
                     houers = int(i[2].split(":")[0])
                     string2 += (
                         f"Кинотеатр: {self.name}\nЗал: {i[0].name}\nВремя:{i[2]}-{houers + i[2].during // 60}:{minutes}")
             bot.send_message(chat_id=message.chat.id, text=string2)
             d = []
+
             for i in time:
                 d.append(telebot.types.KeyboardButton(text=str(i)))
             keyboard3 = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
             keyboard3.add(*d)
             bot.send_message(chat_id=message.chat.id, text="Выбирите время: ", reply_markup=keyboard3)
             target4 = message.text
+
             while not (target4 in time):
                 bot.send_message(chat_id=message.chat.id, text="В это время нет киносеансов. Выбирите время: ", reply_markup=keyboard3)
                 target4 = message.text
+
             string3 = ""
+
             for i in b:
                 if i[1].name == target1 and i[0].name == target3 and i[2] == target4:
                     minutes = int(i[2].split(":")[1])
+
                     if len(str(minutes)) == 1:
                         minutes = str(minutes).replace(str(minutes), "0" + str(minutes))
                     houers = int(i[2].split(":")[0])
                     string3 += (
                         f"Кинотеатр: {i[0].name}\nЗал: {i[1].name}\nВремя:{i[3]}-{houers + i[2].during // 60}:{minutes}")
                     i[1].book(i[3], i[2].name)
+
                     bot.send_message(chat_id=message.chat.id, text=string3)
 
 
 
         else:
-            halss = []
+            hallss = []
+
             for i in self.halls:
-                halss.append(i.name)
+                hallss.append(i.name)
             d = []
-            for i in halls:
+
+            for i in hallss:
                 d.append(telebot.types.KeyboardButton(text=str(i)))
             keyboard4 = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
             keyboard4.add(*d)
             bot.send_message(chat_id=message.chat.id, text="Введите название кинозала: ", reply_markup=keyboard4)
             hall = message.text
-            while not (hall in halls):
-                bot.send_message(chat_id=message.chat.id, text="Такого кинотеатра не сушествует. Введите название кинозала: ", reply_markup=keyboard4)
-                halss = message.text
-            self.halls[halls.index(hall)].book()
+
+            while not (hall in hallss):
+                bot.send_message(chat_id=message.chat.id, text="Такого кинозала не сушествует. Введите название кинозала: ", reply_markup=keyboard4)
+                hallss = message.text
+
+            self.halls[hallss.index(hall)].book()
 
 # def createCinema() -> Cinema:
 #     return Cinema(input("Введите название кинотеатра: "), [])
